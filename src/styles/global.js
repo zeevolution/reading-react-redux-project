ae39f57d9538ec6d9f7e6994568e67c906308579
Jose Lima Neto <jose@basiscomply.com>
2018-10-25 22:30:13 -0300

    style: Add styled components, editorconfig and eslintrc

---
+++ src/styles/global.js
@@ -0,0 +1,22 @@
+import { injectGlobal } from 'styled-components';
+import 'font-awesome/css/font-awesome.css';
+
+injectGlobal`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+    background: #181818;
+  }
+`;
