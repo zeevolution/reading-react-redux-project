5b7ab404c01a12c64f6553622a78a7c714377957
Jose Lima Neto <jose@basiscomply.com>
2018-11-25 20:07:04 -0300

    chore: configure redux with ducks pattern

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
 import "./config/reactotron";
@@ -10,19 +11,22 @@ import Header from "./components/Header/Header";
 import { Wrapper, Container, Content } from "./styles/components";
 
 import Routes from "./routes";
+import store from "./store";
 
 const App = () => (
-  <BrowserRouter>
-    <Wrapper>
-      <Container>
-        <Sidebar />
-        <Content>
-          <Header />
-          <Routes />
-        </Content>
-      </Container>
-    </Wrapper>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Wrapper>
+        <Container>
+          <Sidebar />
+          <Content>
+            <Header />
+            <Routes />
+          </Content>
+        </Container>
+      </Wrapper>
+    </BrowserRouter>
+  </Provider>
 );
 
 export default App;
